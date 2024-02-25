06c2f6f139f78d6ba30820b29f7dcf4a19fcdeef
Levent Cadir <cadirlevent98@gmail.com>
2024-02-25 12:21:14 +0200

    enable cors in backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { customerServiceRouter } from './routers/CustomerService';
 import { topicRouter } from './routers/Topics';
 
@@ -11,6 +12,7 @@ dotenv.config();
 const db = `mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@cluster0.aoz95g5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 mongoose.connect(db).then(() => console.log('Connected to db'));
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/customer-service', customerServiceRouter);
